5b01b1512f0a9cfbf44987a76222a00ccfb654f7
Niyi Enny <myjessy.sr@gmail.com>
2022-11-15 07:23:06 +0100

    Check

--- pages/api/FrontDesk/DeclineSale.tsx
+++ pages/api/FrontDesk/DeclineSale.tsx
@@ -1,12 +1,19 @@
 import { prisma } from "../../../lib/prisma";
 
-export default async (req, res) => {
-  let data = req.body;
-  data = JSON.parse(data);
-  const result = await prisma.declinedSales.createMany({
-    data: {
-      ...data,
-    },
-  });
-  res.status(200).json(result);
-};
+export default async (req:any, res:any) => {
+    const { branch } = req.query
+    const result = await prisma.customer.findMany({
+        where: {
+          branchId: parseInt(branch)
+        },
+        select: {
+          name: true,
+          branchId: true,
+          phone: true,
+          uniqueId: true,
+          customerType: true,
+          purchaseCount: true
+        },
+      });
+      res.status(200).json(result);
+  };
