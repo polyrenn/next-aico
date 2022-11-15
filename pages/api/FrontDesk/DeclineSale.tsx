36a288922a0080e5637fffde3d1abcc2e0ac5787
Niyi Enny <myjessy.sr@gmail.com>
2022-11-15 07:20:10 +0100

    Decline

--- pages/api/FrontDesk/DeclineSale.tsx
+++ pages/api/FrontDesk/DeclineSale.tsx
@@ -1,15 +1,12 @@
 import { prisma } from "../../../lib/prisma";
 
 export default async (req, res) => {
-    let data = req.body;
-    const { id } = req.query
-    data = JSON.parse(data);
-    const branch = data.branch;
-      const result = await prisma.declinedSales.create({
-        data: {
-          ...data,
-        },
-        
-      });
-      res.status(200).json(result);
-  };
+  let data = req.body;
+  data = JSON.parse(data);
+  const result = await prisma.declinedSales.createMany({
+    data: {
+      ...data,
+    },
+  });
+  res.status(200).json(result);
+};
